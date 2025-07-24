8b569f59eca06a7e9cf3fb9a7810efb5d59ed7d1
Dixitsharma47044 <dixitsharma7807047044@gmail.com>
2025-07-24 13:06:06 +0530

    24.07.25

--- Question11.java
+++ Question11.java
@@ -1,7 +1,8 @@
-public class Question10 {
+public class Question11 {
+
     public static void main(String[] args) {
-        int a = 4;
-        int b = 2;
+        int a = 3;
+        int b = 3;
         int c = 1;
 
         int[][] matrix = new int[a][b];
@@ -25,7 +26,24 @@ public class Question10 {
             }
             System.out.println();
         }
+        System.out.println();
+
+        int s[][] = new int[a][b];
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                s[i][j] = 0;
+                for (int k = 0; k < a; k++) {
+                    s[i][j] += matrix[i][k] * t[k][j];
+                }
+            }
+        }
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                System.out.print(s[i][j] + " ");
+            }
+            System.out.println();
+        }
+
     }
 
 }
-
